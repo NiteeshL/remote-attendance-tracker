1e632b03e83b03a2c99b28aaff5553901adbc35a
Arunabha Jana <80163871+arunabhajana@users.noreply.github.com>
2025-03-22 22:07:18 +0530

    Fixing Errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import UserActivity from "./components/UserActivity";
-import LiveAttendancePage from "./pages/LiveAttendancePage";
 import ReportsPage from "./pages/ReportsPage";
 import ProfilePage from "./pages/UserProfile";
-import SettingsPage from "./pages/SettingsPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import Leaderboard from "./pages/Leaderboard";
 import Reports from "./pages/Reports";
@@ -17,10 +15,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/user-activity" element={<UserActivity />} />
-        <Route path="/live-activity" element={<LiveAttendancePage />} />
         <Route path="/report" element={<ReportsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingsPage />} />
         <Route path="/admindashboard" element={<AdminDashboard />} />
         <Route path="/leaderboard" element={<Leaderboard/>} />
         <Route path="/reports" element={<Reports />} />
