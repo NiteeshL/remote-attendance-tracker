825361eda6a94c071dfee34b35b3166b8991be18
NiteeshL <niteeshleela@gmail.com>
2025-03-23 04:10:53 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'localhost',
+      '*.amazonaws.com',
+      'a8e0e8f4578b44efca15e6c346fa517f-c1a78a8970840967.elb.ap-south-1.amazonaws.com'
+    ]
+  }
 })
